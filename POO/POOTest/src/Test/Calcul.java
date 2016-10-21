e685afe86c0e67f2abb146251ba6e1e388a5d5ab
LePingWin <loic.bine@gmail.com>
2016-10-21 15:23:20 +0200

    Ajout de Test et de RunConfig pour le Test

---
+++ POO/POOTest/src/Test/Calcul.java
@@ -0,0 +1,14 @@
+package Test;
+
+/**
+ * Created by pingwin on 21/10/16.
+ */
+public class Calcul {
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String args[]){
+        System.out.println(add(2,3));
+    }
+}
