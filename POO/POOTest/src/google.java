75d791e7fc4964dd4965e14b575dd6a050a5b354
LePingWin <loic.bine@gmail.com>
2016-10-21 11:50:08 +0200

    Ajout de l'exemple Google

---
+++ POO/POOTest/src/google.java
@@ -0,0 +1,40 @@
+/**
+ * Created by pingwin on 21/10/16.
+ */
+import java.net.URL;
+import javax.swing.*;
+import javax.swing.event.*;
+public class google {
+    public static void main(String[] args){
+        final JEditorPane editeur;
+        JPanel pannel = new JPanel();
+        try{
+            editeur = new JEditorPane(new URL("http://google.fr"));
+            editeur.setEditable(false);
+            editeur.addHyperlinkListener(new HyperlinkListener() {
+                @Override
+                public void hyperlinkUpdate(HyperlinkEvent e) {
+                    if(e.getEventType() == HyperlinkEvent.EventType.ACTIVATED){
+                        URL url = e.getURL();
+                        if(url == null)
+                            return;
+                        try{
+                            editeur.setPage(e.getURL());
+                        }catch(Exception ex){
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+            });
+            pannel.add(editeur);
+
+        }catch (Exception e1){
+            e1.printStackTrace();
+        }
+        JFrame f = new JFrame("ma fenetre");
+        f.setSize(800,600);
+        f.getContentPane().add(pannel);
+        f.setVisible(true);
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CNAM_2016_2017
-1ere Année CNAM
+Cours de la 1ere Année CNAM
